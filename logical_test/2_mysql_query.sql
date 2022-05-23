ea8a01fad0ccf3973f5bd4e5018aedb1a5920717
omed28 <santomedia.sm@gmail.com>
2022-05-23 19:33:21 +0700

    Delete 2_mysql_query.sql

--- logical_test/2_mysql_query.sql
+++
@@ -1,7 +0,0 @@
-SELECT A.mhs_id,A.mhs_nim,A.mhs_nama 
-FROM tb_mahasiswa A
-LEFT JOIN tb_mahasiswa_nilai B WHERE B.mhs_id = A.mhs_id
-LEFT JOIN tb_matakuliah C WHERE C.mk_id = b.mk_id
-
-WHERE C.mk_kode = 'MK303'
-ORDER BY C.nilai DESC LIMIT 1
